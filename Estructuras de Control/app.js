225e680718a1715945e3a940a5706a240894d4cf
Jesica Gutiérrez <jesica.gutierrez5@soy.sena.edu.co>
2024-10-19 13:50:44 -0500

    JS

--- Estructuras de Control/app.js
+++ Estructuras de Control/app.js
@@ -265,6 +265,7 @@ if(edadGabriel <=10){
     console.log('Gabriel es un adulto mayor')
 }
 
+
 let edadCarlos = 18;
 
 if(edadCarlos <=10){
@@ -274,10 +275,221 @@ if(edadCarlos <=10){
 } else if ((edadCarlos >=18) && (edadCarlos<60)){
     console.log('Carlos es un adulto');
 }else if((edadCarlos>=60)){
-    console.log('Carlos es un adulto mayor')
+    console.log('Carlos es un adulto mayor')}
 
  
+let edadHenry = 45;
+
+if(edadHenry <=10){
+    console.log('Henry es un niño');
+} else if ((edadHenry>10) && (edadHenry<18)){
+    console.log('Henry es un adolescente');
+} else if ((edadHenry >=18) && (edadHenry<60)){
+    console.log('Henry es un adulto');
+}else if((edadHenry>=60)){
+    console.log('Henry es un adulto mayor')}
+
+
  
- let edadHenry = 45;
  let edadAndrea = 15;
 
+ if (edadAndrea <=10){
+    console.log('Andrea es una niña');
+ } else if ((edadAndrea>10) && (edadAndrea<18)){
+    console.log('Andrea es una adolescente');
+ } else if((edadAndrea >=18) && (edadAndrea <60)){
+    console.log('Andrea es una adulta');
+ } else if ((edadAndrea >=60)){
+    console.log('Andrea es una adulta mayor');
+ }
+
+//Cambiando las edades
+
+edadPablo = 24;
+
+if(edadPablo <=10){
+    console.log('Pablo es un niño');
+} else if ((edadPablo >10) && (edadPablo<18)){
+    console.log('Pablo es un adolescente');
+} else if ((edadPablo >=18) && (edadPablo<60)){
+    console.log('Pablo es un adulto');
+}else if((edadPablo>=60)){
+    console.log('Pablo es un adulto mayor')
+}
+    
+edadGabriel = 95;
+
+if(edadGabriel <=10){
+    console.log('Gabriel es un niño');
+} else if ((edadGabriel>10) && (edadGabriel<18)){
+    console.log('Gabriel es un adolescente');
+} else if ((edadGabriel >=18) && (edadGabriel<60)){
+    console.log('Gabriel es un adulto');
+}else if((edadGabriel>=60)){
+    console.log('Gabriel es un adulto mayor')
+}
+
+
+edadCarlos = 1;
+
+if(edadCarlos <=10){
+    console.log('Carlos es un niño');
+} else if ((edadCarlos>10) && (edadCarlos<18)){
+    console.log('Carlos es un adolescente');
+} else if ((edadCarlos >=18) && (edadCarlos<60)){
+    console.log('Carlos es un adulto');
+}else if((edadCarlos>=60)){
+    console.log('Carlos es un adulto mayor')}
+
+ 
+edadHenry = 17.5;
+
+if(edadHenry <=10){
+    console.log('Henry es un niño');
+} else if ((edadHenry>10) && (edadHenry<18)){
+    console.log('Henry es un adolescente');
+} else if ((edadHenry >=18) && (edadHenry<60)){
+    console.log('Henry es un adulto');
+}else if((edadHenry>=60)){
+    console.log('Henry es un adulto mayor')}
+
+
+ 
+ edadAndrea = 59.9;
+
+ if (edadAndrea <=10){
+    console.log('Andrea es una niña');
+ } else if ((edadAndrea>10) && (edadAndrea<18)){
+    console.log('Andrea es una adolescente');
+ } else if((edadAndrea >=18) && (edadAndrea <60)){
+    console.log('Andrea es una adulta');
+ } else if ((edadAndrea >=60)){
+    console.log('Andrea es una adulta mayor');
+ }
+
+ /*Ejercicio 2
+
+ - Luis:5
+ - Carlos 9.5
+ - Camila 6.9
+ - Ricardo 8
+ - Luisa 9.2 
+ - Andrés 3
+
+ Condiciones:
+ - Entre 0 y 3: Muy deficiente
+ - Más de 3 pero menos de 5: Insuficiente
+ - Entre 5 y 6: Suficiente
+ - Más de 6 pero menos de 7: Bien
+ - Entre 7 y 9: Notable
+ - Más de nueve hasta 10: Sobresaliente */
+
+ // Calificaciones de los estudiantes
+var luis = 5;
+var carlos = 9.5;
+var camila = 6.9;
+var ricardo = 8;
+var luisa = 9.2;
+var andres = 3;
+
+// Clasificación de Luis
+if (luis >= 0 && luis <= 3) {
+    console.log('La nota de Luis es: Muy deficiente');
+} else if (luis > 3 && luis < 5) {
+    console.log('La nota de Luis es: Insuficiente');
+} else if (luis >= 5 && luis <= 6) {
+    console.log('La nota de Luis es: Suficiente');
+} else if (luis > 6 && luis < 7) {
+    console.log('La nota de Luis es: Bien');
+} else if (luis >= 7 && luis <= 9) {
+    console.log('La nota de Luis es: Notable');
+} else if (luis > 9 && luis <= 10) {
+    console.log('La nota de Luis es: Sobresaliente');
+} else {
+    console.log('La nota de Luis es: Nota no válida');
+}
+
+// Clasificación de Carlos
+if (carlos >= 0 && carlos <= 3) {
+    console.log('La nota de Carlos es: Muy deficiente');
+} else if (carlos > 3 && carlos < 5) {
+    console.log('La nota de Carlos es: Insuficiente');
+} else if (carlos >= 5 && carlos <= 6) {
+    console.log('La nota de Carlos es: Suficiente');
+} else if (carlos > 6 && carlos < 7) {
+    console.log('La nota de Carlos es: Bien');
+} else if (carlos >= 7 && carlos <= 9) {
+    console.log('La nota de Carlos es: Notable');
+} else if (carlos > 9 && carlos <= 10) {
+    console.log('La nota de Carlos es: Sobresaliente');
+} else {
+    console.log('La nota de Carlos es: Nota no válida');
+}
+
+// Clasificación de Camila
+if (camila >= 0 && camila <= 3) {
+    console.log('La nota de Camila es: Muy deficiente');
+} else if (camila > 3 && camila < 5) {
+    console.log('La nota de Camila es: Insuficiente');
+} else if (camila >= 5 && camila <= 6) {
+    console.log('La nota de Camila es: Suficiente');
+} else if (camila > 6 && camila < 7) {
+    console.log('La nota de Camila es: Bien');
+} else if (camila >= 7 && camila <= 9) {
+    console.log('La nota de Camila es: Notable');
+} else if (camila > 9 && camila <= 10) {
+    console.log('La nota de Camila es: Sobresaliente');
+} else {
+    console.log('La nota de Camila es: Nota no válida');
+}
+
+// Clasificación de Ricardo
+if (ricardo >= 0 && ricardo <= 3) {
+    console.log('La nota de Ricardo es: Muy deficiente');
+} else if (ricardo > 3 && ricardo < 5) {
+    console.log('La nota de Ricardo es: Insuficiente');
+} else if (ricardo >= 5 && ricardo <= 6) {
+    console.log('La nota de Ricardo es: Suficiente');
+} else if (ricardo > 6 && ricardo < 7) {
+    console.log('La nota de Ricardo es: Bien');
+} else if (ricardo >= 7 && ricardo <= 9) {
+    console.log('La nota de Ricardo es: Notable');
+} else if (ricardo > 9 && ricardo <= 10) {
+    console.log('La nota de Ricardo es: Sobresaliente');
+} else {
+    console.log('La nota de Ricardo es: Nota no válida');
+}
+
+// Clasificación de Luisa
+if (luisa >= 0 && luisa <= 3) {
+    console.log('La nota de Luisa es: Muy deficiente');
+} else if (luisa > 3 && luisa < 5) {
+    console.log('La nota de Luisa es: Insuficiente');
+} else if (luisa >= 5 && luisa <= 6) {
+    console.log('La nota de Luisa es: Suficiente');
+} else if (luisa > 6 && luisa < 7) {
+    console.log('La nota de Luisa es: Bien');
+} else if (luisa >= 7 && luisa <= 9) {
+    console.log('La nota de Luisa es: Notable');
+} else if (luisa > 9 && luisa <= 10) {
+    console.log('La nota de Luisa es: Sobresaliente');
+} else {
+    console.log('La nota de Luisa es: Nota no válida');
+}
+
+// Clasificación de Andrés
+if (andres >= 0 && andres <= 3) {
+    console.log('La nota de Andrés es: Muy deficiente');
+} else if (andres > 3 && andres < 5) {
+    console.log('La nota de Andrés es: Insuficiente');
+} else if (andres >= 5 && andres <= 6) {
+    console.log('La nota de Andrés es: Suficiente');
+} else if (andres > 6 && andres < 7) {
+    console.log('La nota de Andrés es: Bien');
+} else if (andres >= 7 && andres <= 9) {
+    console.log('La nota de Andrés es: Notable');
+} else if (andres > 9 && andres <= 10) {
+    console.log('La nota de Andrés es: Sobresaliente');
+} else {
+    console.log('La nota de Andrés es: Nota no válida');
+}
